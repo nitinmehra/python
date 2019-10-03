866932c06e5f7a44c4cd5da42ab3833681bd8ef7
Delta114514 <gey3933@gmail.com>
2019-10-03 23:46:35 +0900

    maybe this is what you want to do :)

--- practice_programs.py
+++ practice_programs.py
@@ -5,11 +5,11 @@
 #       55555
 
 # normal way
-for i in range(1,6):
-    for j in range(0,i):
+for i in range(1, 5):
+    for j in range(i):
         print(i, end="")
     print('')
 #lambda
 
-pattern_1 = lambda number: for i in range(0,number): print(i)
+pattern_1 = lambda number: [print(str(i)*i) for i in range(1, number)][0]
 pattern_1(5)
